560f2bafd0607e50537eef8cc48816c6d05da7c6
kidloserme <love99love>
2016-06-11 20:50:57 +0800

    添加版本升级提示

--- Android/app/src/main/java/org/easydarwin/easyclient/activity/MainActivity.java
+++ Android/app/src/main/java/org/easydarwin/easyclient/activity/MainActivity.java
@@ -6,11 +6,18 @@
 */
 package org.easydarwin.easyclient.activity;
 
+import android.app.DownloadManager;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -24,11 +31,13 @@ import org.easydarwin.easyclient.R;
 import org.easydarwin.easyclient.adapter.OnlineCameraAdapter;
 import org.easydarwin.easyclient.callback.DeviceInfoCallback;
 import org.easydarwin.easyclient.callback.LiveVOCallback;
+import org.easydarwin.easyclient.callback.VersionCallback;
 import org.easydarwin.easyclient.config.DarwinConfig;
 import org.easydarwin.easyclient.domain.Device;
 import org.easydarwin.easyclient.domain.DeviceHeader;
 import org.easydarwin.easyclient.domain.DeviceInfoWrapper;
 import org.easydarwin.easyclient.domain.LiveVO;
+import org.easydarwin.easyclient.domain.RemoteVersionInfo;
 import org.easydarwin.okhttplibrary.OkHttpUtils;
 
 import java.util.ArrayList;
@@ -40,6 +49,7 @@ import okhttp3.Request;
 public class MainActivity extends BaseActivity implements
         AdapterView.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener {
 
+    private static final String TAG="Easy_MainActivity";
     ListView listView;
     OnlineCameraAdapter liveVOAdapter;
     SwipeRefreshLayout swipeRefreshLayout;
@@ -67,6 +77,7 @@ public class MainActivity extends BaseActivity implements
         swipeRefreshLayout.setOnRefreshListener(this);
 
         getDevices();
+        checkUpdate();
     }
 
     private void getDevices() {
@@ -165,6 +176,66 @@ public class MainActivity extends BaseActivity implements
                 startActivity(intent);
             }
         });
+    }
+
+    /**
+     * 检测当前APP是否需要升级
+     */
+    private void checkUpdate(){
+        String url="http://www.easydarwin.org/versions/easyclient/version.txt";
+        OkHttpUtils.get().url(url).build().execute(new VersionCallback(){
+            @Override
+            public void onError(Call call, Exception e) {
+                Log.e(TAG,"Check update faill ...");
+            }
+            @Override
+            public void onResponse(RemoteVersionInfo response) {
+                if(response == null && TextUtils.isEmpty(response.getUrl())){
+                    return;
+                }
+                PackageManager packageManager=getPackageManager();
+                try {
+                    PackageInfo packageInfo=packageManager.getPackageInfo(getPackageName(),0);
+                    int localVersionCode=packageInfo.versionCode;
+                    int remoteVersionCode= Integer.valueOf(response.getVersionCode());
+                    if(localVersionCode<remoteVersionCode){
+                        showUpdateDialog(response.getUrl());
+                    }
+                } catch (PackageManager.NameNotFoundException e) {
+                    Log.e(TAG,"Get PackageInfo error !");
+                }
+            }
+        });
+    }
+
+    /**
+     * 提示升级对话框
+     */
+    private void showUpdateDialog(String linkUrl){
+       final String apkUrl=linkUrl;
+        new AlertDialog.Builder(this)
+                .setMessage("EasyClient可以升级到更高的版本，是否升级")
+                .setTitle("升级提示")
+                .setIcon(R.drawable.client_logo)
+                .setPositiveButton("升级", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                        Uri uri = Uri.parse(apkUrl);
+                        DownloadManager.Request request = new DownloadManager.Request(uri);
+                        // 设置允许使用的网络类型，这里是移动网络和wifi都可以
+                        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE | DownloadManager.Request.NETWORK_WIFI);
+                        // 禁止发出通知，既后台下载，如果要使用这一句必须声明一个权限：android.permission.DOWNLOAD_WITHOUT_NOTIFICATION
+                        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
+                        downloadManager.enqueue(request);
+                        dialog.dismiss();
+                    }
+                }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create().show();
     }
 
     /**
---
+++ Android/app/src/main/java/org/easydarwin/easyclient/callback/VersionCallback.java
@@ -0,0 +1,22 @@
+package org.easydarwin.easyclient.callback;
+
+import com.google.gson.Gson;
+
+import org.easydarwin.easyclient.domain.RemoteVersionInfo;
+import org.easydarwin.okhttplibrary.callback.Callback;
+
+import okhttp3.Call;
+import okhttp3.Response;
+
+/**
+ * Created by dell on 2016/6/11 011.
+ */
+public abstract class VersionCallback extends Callback<RemoteVersionInfo> {
+    @Override
+    public RemoteVersionInfo parseNetworkResponse(Response response) throws Exception {
+        String string = response.body().string();
+        RemoteVersionInfo versionInfo = new Gson().fromJson(string, RemoteVersionInfo.class);
+        return versionInfo;
+    }
+
+}
